4b719ab03093dfb9a8ca01ca5751a0e62f6de24b
kenxx16 <40766947+kenxx16@users.noreply.github.com>
2020-03-24 13:50:17 +0300

    карта
    вывод данных по станции

--- js/leaflet/leaflet-map.js
+++ js/leaflet/leaflet-map.js
@@ -1,5 +1,5 @@
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function(){
 
   if (document.getElementById('mapid')) {
 
@@ -29,9 +29,90 @@ document.addEventListener("DOMContentLoaded", () => {
       mymap.panInsideBounds(bounds, {animate: false});
     });
 
+    let tr = {
+      cname: "Описание станции",
+      name: "Название станции",
+      organization: "Организация",
+      network: "Сеть (RUS)",
+      network_en: "Сеть (LAT)",
+      id_network: "Номер в сети",
+      conditional_name_rus: "Условное название (RUS)",
+      conditional_name_lat: "Условное название (LAT)",
+      date_install: "Дата установки",
+      destination: "Назначение",
+      department: "Принадлежность",
+      mode: "Режим функционирования",
+      altitude: "Высотная привязка",
+      address:{
+        cname: "Адресс",
+      	region: "Регион",
+      	city: "Город",
+      	street: "Улица",
+      	description: "Расположение"
+      },
+      contact: {
+        cname: "Контакты",
+      	operating_organization: "Организация",
+      	person_name: "Контактное лицо",
+      	person_position: "Должность",
+      	tel: "Телефон",
+      	email: "Email"
+      },
+      metrology: "Сведения о метрологии",
+      meteo_equipment: "Метеорологическая аппаратура",
+      placement_equipment: "Размещение спутниковой аппаратуры",
+      log: "Лог-файл",
+      coord: {
+        cname: "Координаты",
+      	B: "B",
+      	L: "L",
+      	H: "H",
+      	Hg: "Hg",
+      	X: "X",
+      	Y: "Y",
+      	Z: "Z"
+      },
+      station_center: {
+        cname: "Центр станции",
+      	antenna: {
+          cname: "Антенна",
+      		name: "Название",
+      		manufacturer: "Производитель",
+      		serial_number: "Серийный номер"
+      	},
+      	receiver: {
+          cname: "Приёмник",
+      		name: "Название",
+      		altname: "Сокращённое название",
+      		manufacturer: "Производитель",
+      		serial_number: "Серийный номер"
+      	},
+        number_of_channels: "Количество каналов",
+        frequency_standard: "Внешний стандарт частоты",
+      	chocke_ring: "ГНСС антенна Chocke Ring",
+      	nearby_geodetic_points: "Близлежащие геодезические пункты",
+      	placement: "Условия размещения станции",
+      	type: "Тип центра",
+      	heigh_m: "Высота конструкционного центра (м)",
+        cable_length_m: "Длинна кабеля (м)",
+        gnss: "Принимаемые спутниковые системы",
+      	frequency: {
+          cname: "Частоты",
+      		Beidou: "Beidou",
+      		COMPASS: "COMPASS",
+      		GLONASS: "GLONASS",
+      		GPS: "GPS",
+      		Galileo: "Galileo",
+      		IRNSS: "IRNSS",
+      		QZSS: "QZSS",
+      		SBAS: "SBAS"
+      	}
+      }
+    };
+
+    tr.station_ceneter = tr.station_center;
 
     let arMarker = {
-
       "RZD": L.divIcon({
         className: 'mdi mdi-radiobox-marked rzd',
         iconSize: [24, 24],
@@ -78,13 +159,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
     (async () => {
       let markersClusterGroup = L.markerClusterGroup.layerSupport();
-
-
       let response = await fetch(apiURL + '/getStations/');
       let dat = await response.json();    //  dat[ID, net, coord]
-
-
-
       let marker;
       let net = [];
 
@@ -121,6 +197,83 @@ document.addEventListener("DOMContentLoaded", () => {
 
       markersClusterGroup.checkIn(net);
 
+      function prim(text) {
+        return "<span class=\"badge badge-primary\">"+text+"</span> "
+      }
+
+      function pil(text) {
+        return "<span class=\"badge badge-pill badge-secondary\">"+text+"</span> "
+      }
+
+      function output(ob, ha) {
+        let outInfo = "<div class=\"card text-dark border-dark mb-1 mt-1 InfoCard\"> <div class=\"card-header p-2\"> <h1>" + ha["cname"] + "</h1> </div> <div class=\"row no-gutters\"><div class=\"card-body p-2\">";
+        let sc = "";
+        for (let v in ha) {
+          if (!ob[v] || v == 'number' || v == 'ID') {
+            continue;
+          }
+
+          if (v=="station_center"||v=="station_ceneter") {
+            sc+=output(ob[v], ha[v]);
+            continue;
+          }
+
+          if (v == 'chocke_ring') {
+            if (ob[v] == true) {
+              outInfo += prim(ha[v] + ":") + "есть<br><hr>";
+            }
+            else{
+              outInfo += prim(ha[v] + ":") + "нет<br><hr>";
+            }
+            continue;
+          }
+
+          if (v == 'gnss') {
+            let gnss = "";
+            for (let i = 0; i < ob[v].length; i++) {
+              gnss += ob[v][i];
+              if (i!=ob[v].length-1) {
+                gnss+=", ";
+              }
+            }
+            outInfo += prim(ha["gnss"] + ":") + gnss + "<br>";
+            continue;
+          }
+
+          if (v == 'coord' || v == 'frequency') {
+            let coord = "<div class=\"card text-white border-dark mb-1 mt-1 InfoCard\"> <div class=\"card-header p-2\"> <h1>" + ha[v]["cname"] +
+                        "</h1> </div> <div class=\"row no-gutters\"><div class=\"card-body p-0\">" +
+                        "<table class=\"table table-dark table-hover mb-0\"> <tbody>";
+            for (let i in ob[v]){
+              coord += "<tr><th scope=\"row\">" + i + "<\/th><td class=\"w-50\">" + ob[v][i] + "</td></tr>";
+            };
+            coord += "</tbody></table></div></div></div>";
+            outInfo += coord;
+            continue;
+          }
+
+          if (v == 'address' || v == 'contact') {
+            let address = prim(ha[v]["cname"] + ":");
+            for (let w in ha[v]) {
+              if (!ob[v][w]) {
+                continue;
+              }
+              address += pil(ha[v][w] + ":") + ob[v][w] + " ";
+            }
+            outInfo += address + "<br><hr>";
+            continue;
+          }
+
+          if (typeof ob[v] === 'object') {
+            outInfo += output(ob[v], ha[v]);
+          }
+          else {
+            outInfo += prim(ha[v] + ":") + ob[v] + "<br><hr>";
+          }
+        }
+        outInfo += "</div></div></div>";
+        return(outInfo + sc);
+      }
 
       function popupOpen(pop) {
         this.currentPop = pop;
@@ -143,32 +296,23 @@ document.addEventListener("DOMContentLoaded", () => {
           let station = await fetch(apiURL + '/getStation/?id=' + pop.target._leaflet_id + '&lang=' + lang);
           let fullDat = await station.json();
 
-          p = document.getElementById("name").innerHTML = "Название: " + fullDat.name;
+          p = document.getElementById("name").innerHTML = fullDat.name;
           p = document.getElementById("ID").innerHTML = "ID: " + fullDat.ID;
-          p = document.getElementById("organization").innerHTML = "Организация: " + fullDat.organization + "<hr>";
-          p = document.getElementById("network").innerHTML = "Сеть: " + fullDat.network + "<hr>";
-          p = document.getElementById("id_network").innerHTML = "ID в сети: " + fullDat.id_network + "<hr>";
-          p = document.getElementById("conditional_name_rus").innerHTML = "Условное название (RUS): " + fullDat.conditional_name_rus + "<hr>";
-          p = document.getElementById("conditional_name_lat").innerHTML = "Условное название (LAT): " + fullDat.conditional_name_lat + "<hr>";
-          p = document.getElementById("date_install").innerHTML = "Дата установки: " + fullDat.date_install + "<hr>";
-          p = document.getElementById("destination").innerHTML = "Назначение: " + fullDat.destination + "<hr>";
-          p = document.getElementById("department").innerHTML = "Принадлежность: " + fullDat.department + "<hr>";
-          p = document.getElementById("address").innerHTML = "Адрес: " + fullDat.address.city + ", " + fullDat.address.street + "<hr>";
 
+          let outInfo = output(fullDat, tr);
+          p = document.getElementById("AllInfo").innerHTML = outInfo;
 
           let statPic = await fetch('http://test.lan/AllPic.php?pk_id=' + pop.target._leaflet_id + '&target=map')
           let jsonPic = await statPic.json();
           console.log(jsonPic);
           if (jsonPic.length!=0) {
-            document.SatImg.src = 'http://test.lan/getIMG.php?pk_id=' + jsonPic[0];
+            document.getElementById("SatImg").src = 'http://test.lan/getIMG.php?pk_id=' + jsonPic[0];
+            document.getElementById("SatImg1").src = 'http://test.lan/getIMG.php?pk_id=' + jsonPic[0];
           }
           else {
-            document.SatImg.src = 'assets/img/favicon.png';
+            document.getElementById("SatImg").src = 'assets/img/favicon.png';
+            document.getElementById("SatImg1").src = 'assets/img/favicon.png';
           }
-
-
-
-
           pop.target.bindPopup("Название: " + fullDat.name + "<br/>ID: " + fullDat.ID);
 
         })();
